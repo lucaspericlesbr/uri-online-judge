6a15ccafac8a1eea0267505530ed2fcdbe28c029
lucaspericlesbr <lucaspericlesbr@gmail.com>
2021-05-20 20:53:35 -0300

    Print Hello World.

--- Ex001-HelloWorld.py
+++ Ex001-HelloWorld.py
@@ -1,11 +1,9 @@
-'''
-Your first program in any programming language is usually "Hello World!". In this first problem all you have to do is print this message on the screen.
+'''Your first program in any programming language is usually "Hello World!". In this first problem all you have to do is print this message on the screen.
 
 Input
 This problem has no input.
 
 Output
-You must print the message "Hello World!" as shown below.
-'''
+You must print the message "Hello World!" as shown below.'''
 
 print('Hello World!')
