8f65559376813c98b961d679147b5a6e0de9b90e
lucaspericlesbr <lucaspericlesbr@gmail.com>
2021-05-20 21:08:24 -0300

    Simple sum of two integers.

--- Ex004-SimpleSum.py
+++ Ex004-SimpleSum.py
@@ -1,4 +1,6 @@
 '''
+#1003
+
 Read two integer values, in this case, the variables A and B. After this, calculate the sum between them and assign it to the variable SOMA. Write the value of this variable.
 Input
 
